577f6968cbbd5da5d75b02d61e904127605e4a12
PixelAgent007 <oskar.manhart@gmx.de>
2022-01-22 09:55:25 +0100

    Rewrote event handler and added bump reminder

--- src/listener/ready.js
+++ src/listener/ready.js
@@ -1,9 +1,13 @@
-module.exports = {
-    name: "ready",
+// Imports
+const Listener = require("../lib/listener.js");
+
+module.exports = new Listener({
+    name: "Bot Startup",
+    type: "ready",
     once: true,
-    execute() {
+    run() {
         // Clear console
         console.clear();
         console.log("Bot connected to Discord!");
     },
-};
+});
