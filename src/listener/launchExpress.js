091f5a9bdca2ddb010745477ebd49c597e0af526
PixelAgent007 <oskar.manhart@gmx.de>
2022-03-14 17:28:37 +0100

    Added CORS entry

--- src/listener/launchExpress.js
+++ src/listener/launchExpress.js
@@ -34,7 +34,7 @@ module.exports = new Listener({
         });
 
         app.use(function (req, res, next) {
-            res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+            res.header("Access-Control-Allow-Origin", "apply.oskar.global");
             res.header(
                 "Access-Control-Allow-Headers",
                 "Origin, X-Requested-With, Content-Type, Accept"
